995e8f20c6ba7fba7022b961aad162933bd5ae1e
Agowert <70849596+Agowert@users.noreply.github.com>
2024-07-04 14:58:35 +0300

    gg

--- partneram/raspisanie-meropriyatiy/index.php
+++ partneram/raspisanie-meropriyatiy/index.php
@@ -1,5 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("description", "Партнерам. Как стать партнером лидера отрасли");
 $APPLICATION->SetTitle("Расписание мероприятий");
 ?>
 
